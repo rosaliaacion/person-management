af4412ab234b2208554e09ddaf34fd37689250b0
rosaliaacion <95088900+rosaliaacion@users.noreply.github.com>
2021-12-05 13:26:24 -0300

    Creating person component (#1)
    Adding the Person component to app

--- src/components/Person/index.jsx
+++ src/components/Person/index.jsx
@@ -1,5 +1,18 @@
-const Person = () => {
-    return <>Componente Person</>
+const Person = (props) => {
+
+    const personFullName = () => {
+        return `${props.person.name} ${props.person.last_name}`; //`` backtips comillas invertidas para crear string e interpolar valores dinamicos dentro de una cadena
+    }
+
+    return <>
+            <div className="Person">
+                <div>{personFullName()}</div>
+                <div>{props.person.age} Años</div>
+                <div>Sexo: {props.person.sex}</div>
+                <div>{props.person.country}</div>
+                <div>{props.person.ci}</div>
+            </div>
+        </>
 }
 
 export default Person;
