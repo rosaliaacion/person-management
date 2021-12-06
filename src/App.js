29b80f83e18bb1d65cbd38e64d62489ad8456503
rosaliaacion <95088900+rosaliaacion@users.noreply.github.com>
2021-12-06 19:54:03 -0300

    Dayan/feature/person list component (#2)
    * Progress
    
    -Starting the PersonList component.
    -Adding the PersonList component to the App using dummy data to test proposal.
    -Updating the App.css style file.
    
    * Progress
    
    -Adding state to App.js to manage persons dynamically.
    
    Co-authored-by: Dayan Perez <dayan.perez@autodesk.com>

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import './App.css';
 
 //Importing Components here
@@ -5,14 +7,21 @@ import PersonList from "./components/PersonList";
 import NewPerson from './components/NewPerson';
 
 function App() {
+
+  const [persons, setPersons] = useState([]);
+
+  const onNewPersonAddHandler = (person) => {
+    setPersons([...persons, person])
+  }
+
   return (
     <div className="App">
       <div className="App-Container">
         <div className="App-New-Person-Container">
-          <NewPerson onClickNewPerson={(payload) => console.log(payload)}/>
+          <NewPerson onClickNewPerson={onNewPersonAddHandler}/>
         </div>
         <div className="App-Person-List-Container">
-          <PersonList />
+          <PersonList persons={persons} onDelete={(index) => console.log(index)}/>
         </div>
       </div>
     </div>
--- src/components/PersonList/index.jsx
+++ src/components/PersonList/index.jsx
@@ -1,5 +1,18 @@
-const PersonList = () => {
-    return <>Componente PersonList</>
+import Person from "../Person";
+
+const PersonList = (props) => {
+    return <>
+            <div className="PersonList">
+                {props.persons.length > 0 && 
+                    <>
+                        {props.persons.map((person, index) => {
+                            return <Person key={index} person={person} />
+                        })}
+                    </>
+                }
+                {props.persons.length === 0 && <>No persons here yet.</>}
+            </div>
+        </>
 }
 
 export default PersonList;
