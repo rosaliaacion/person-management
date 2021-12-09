ffcd0505e07ce4c8ec27bcbc9b5c7cddb1854a69
rosaliaacion <95088900+rosaliaacion@users.noreply.github.com>
2021-12-09 20:55:07 -0300

    rosi/feature/customs-style-components (#5)
    -Creating Input and Select customs components.
    
    Co-authored-by: Dayan Perez <dayan.perez@autodesk.com>

---
+++ src/components/Commons/Select/index.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Select = styled.select`
+padding: 10px;
+border: 1px solid #ddd;
+border-radius: 5px;
+margin: 5px;
+width: 27%;
+`;
+
+export default Select
